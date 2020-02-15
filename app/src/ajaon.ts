b6803e23ca3a44cc68e59114b4aab656bfd570ab
Maximilian Mairinger <maximilian.mairinger@gmx.at>
2020-02-15 15:45:37 +0100

    fail request even when not verbose & sessKey in localStorage is optional

--- app/src/ajaon.ts
+++ app/src/ajaon.ts
@@ -30,14 +30,19 @@ const dir = "/"
 const setVerboseFalseNote = "\n\nIf this behaviour is intended and youd like to disable this warning, set verbose to false."
 const postString = "POST"
 
-function constructConsoleType(type: "warn" | "error" | "log" | ((msg: string) => void)) {
-  if (typeof type === "string") return function constructConsoleVerbose(verbose: boolean) {
+function constructConsoleType(type: "warn" | "error" | "log" | ((msg: string) => void), alwaysExecute?: (msg: string) => void) {
+  const defaultAlwaysExecute = alwaysExecute
+  if (typeof type === "string") return function constructConsoleVerbose(verbose: boolean, alwaysExecute: (msg: string) => void = defaultAlwaysExecute) {
     return function(msg: string) {
-      if (verbose) console[type](msg + setVerboseFalseNote)
+      msg += setVerboseFalseNote
+      alwaysExecute(msg)
+      if (verbose) console[type](msg)
     }
   }
-  else return function constructConsoleVerbose(verbose: boolean) {
-    return function(msg: string) {
+  else return function constructConsoleVerbose(verbose: boolean, alwaysExecute: (msg: string) => void = defaultAlwaysExecute) {
+    return function(msg: string, alwaysExecute?: (msg: string) => void) {
+      msg += setVerboseFalseNote
+      alwaysExecute(msg)
       if (verbose) type(msg + setVerboseFalseNote)
     }
   }
@@ -74,7 +79,7 @@ const constructConsoleWarnVerbose = constructConsoleType("warn")
 type GenericObject = {[key: string]: any} & {[key: number]: any}
 type SessKeyKey = {sessKeyKeyForLocalStorage: string, sessKeyKeyForApi: string}
 
-export default function ajaon(apiUrl: string, sessKeyKey: string | SessKeyKey = "sessKey", verbose: boolean = true) {
+export default function ajaon(apiUrl: string, sessKeyKey?: string | SessKeyKey, verbose: boolean = true) {
   const defaultVervose = verbose
   let warn = constructConsoleWarnVerbose(verbose)
 
@@ -87,21 +92,22 @@ export default function ajaon(apiUrl: string, sessKeyKey: string | SessKeyKey = 
     apiUrl = "https://" + apiUrl
   }
 
-  const sess: SessKeyKey = typeof sessKeyKey === "string" ? {sessKeyKeyForLocalStorage: sessKeyKey, sessKeyKeyForApi: sessKeyKey} : clone(sessKeyKey)
+  const sess: SessKeyKey = sessKeyKey !== undefined ? typeof sessKeyKey === "string" ? {sessKeyKeyForLocalStorage: sessKeyKey, sessKeyKeyForApi: sessKeyKey} : clone(sessKeyKey) : false
   function post<Res = GenericObject>(url: string | string[], body: object | string, headers: HeadersInit | Headers = {'Content-Type': 'application/json'}, verbose: boolean = defaultVervose) {
     return new AjaonPromise<Res, string>(async (res, fail) => {
       headers = headers instanceof Headers ? headers : new Headers(headers)
 
 
-      const error = constructConsoleType((r) => {fail(r); console.error(r)})(verbose)
+      const error = constructConsoleType(console.error, fail)(verbose)
       if (verbose !== defaultVervose) warn = constructConsoleWarnVerbose(verbose)
       
 
       body = typeof body === "string" ? JSON.parse(body) : body
-      if (body[sess.sessKeyKeyForApi] !== undefined) {
-        warn("Session key property \"" + sess.sessKeyKeyForApi + "\" in post body defined as \"" + body[sess.sessKeyKeyForApi] + "\". The sesskey (saved as \"" + sess.sessKeyKeyForLocalStorage + "\" in localStorage) will not be ijected into the payload.");
+      if (sess) {
+        if (body[sess.sessKeyKeyForApi] !== undefined) warn("Session key property \"" + sess.sessKeyKeyForApi + "\" in post body defined as \"" + body[sess.sessKeyKeyForApi] + "\". The sesskey (saved as \"" + sess.sessKeyKeyForLocalStorage + "\" in localStorage) will not be ijected into the payload.");
+        else if (localStorage[sess.sessKeyKeyForLocalStorage] !== undefined) body[sess.sessKeyKeyForApi] = localStorage[sess.sessKeyKeyForLocalStorage]
+        else error("No sessionKey found on the client under " + sess.sessKeyKeyForLocalStorage + ".")
       }
-      else body[sess.sessKeyKeyForApi] = localStorage[sess.sessKeyKeyForLocalStorage] || "";
 
       body = JSON.stringify(body)
 
@@ -147,12 +153,11 @@ export default function ajaon(apiUrl: string, sessKeyKey: string | SessKeyKey = 
 
   async function get<Res>(...url: string[]) {
     return new AjaonPromise<Res, string>(async (res, fail) => {
-      const error = constructConsoleType((r) => {fail(r); console.error(r)})(verbose)
       try {
         res(await (await fetch(validateURL(url))).json())
       }
       catch(e) {
-        error("GET request failed at \"" + validateURL(url) + "\".");
+        constructConsoleType(console.error, fail)(verbose)("GET request failed at \"" + validateURL(url) + "\".");
       }
     })
   }
